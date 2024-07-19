82d0ae53cb3080aa987d7f6309dc5379cbfc4006
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-07-19 10:35:41 +0530

    navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-nvabar")
     document.body.classList.toggle("overflow-hidden")
+    document.getElementById("lines").classList.toggle("line-1-1")
+    document.getElementById("lines-2").classList.toggle("line-2")
+    document.getElementById("lines-3").classList.toggle("line-3")
 }
